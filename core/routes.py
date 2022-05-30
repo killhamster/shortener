55c52787a456d15c6ca5b1d7a567a87b576f39c6
KH <killhamster@gmail.com>
2022-05-30 14:33:07 -0700

    slight updates and requirements

--- core/routes.py
+++ core/routes.py
@@ -19,14 +19,17 @@ def index():
         url = request.form['url']
         short_id = request.form['custom_id']
 
+        # Ensure that the custom short ID isn't already in use
         if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
             flash('That\'s already in use! Please enter different custom id.')
             return redirect(url_for('index'))
 
+        # Ensure there is a URL entered
         if not url:
             flash('The URL is required! Don\'t leave this blank.')
             return redirect(url_for('index'))
 
+        # Ensure that the URL isn't malformed or not a URL
         if uri_validator(url) == False:
             flash('This doesn\'t look like a valid URL!')
             return redirect(url_for('index'))
